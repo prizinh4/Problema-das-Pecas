058207a3c800368abfe7683ef3992d3ffdebe309
Priscila <primoraesandrade57@gmail.com>
2025-10-28 12:54:21 -0300

    add função que lê o arquivo de peças

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def lerPecas(nomeArq):
+    pecas = []
+    with open(nomeArq, 'r') as arquivo:
+        qtdePeças = int(arquivo.readline())
+        for _ in range(qtdePeças):
+            altura, comprimento = map(int, arquivo.readline().split())
+            pecas.append((altura, comprimento))
+    return pecas
